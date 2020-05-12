2ec80d21f69a3f3b49805da1922d260b91a90455
Ashish Thapa <ashish.thapa477@gmail.com>
2020-05-12 13:32:49 +0545

    Added  punctuation support. New Font uploaded fixed figure div element non rendering issue.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,10 @@ Vue.component("generator-form", {
         return {
             content: "",
             kermetFactor: 20,
-            textflyFactor: 0
+            textflyFactor: 0,
+            fontSize: 40,
+            allowSynonyms: "no",
+            showIndent: "white",
         }
     },
     methods: {
@@ -12,7 +15,10 @@ Vue.component("generator-form", {
             const resource = {
                 content: this.content,
                 kermetFactor: this.kermetFactor,
-                textflyFactor: this.textflyFactor
+                textflyFactor: this.textflyFactor,
+                fontSize: this.fontSize,
+                allowSynonyms: this.allowSynonyms,
+                showIndent: this.showIndent
             }
             this.$emit("submitted-elements", resource);
         }
@@ -27,16 +33,16 @@ new Vue({
     data() {
         return {
             resource: "",
-            renderables:"",
-            removeForm:false
+            renderables: "",
+            removeForm: false
         }
     },
     methods: {
         prepareContent(e) {
             this.resource = e;
             let skipPosition = this.regexMatch();
-            
             let renderables = this.splitStrings(skipPosition);
+            this.kermet(this.resource.allowSynonyms, skipPosition);
             this.renderables = renderables.join("");
         },
 
@@ -61,7 +67,7 @@ new Vue({
                     switch (match.value[1]) {
                         case "figure":
                             //replace matched part with actual div element
-                            divElem = `<div style="display:block;background-color:yellow;width:${match.value[2]};height:${match.value[3]}">ssd</div>`;
+                            divElem = `<div style="position:relative;display:block;background-color:${this.resource.showIndent};width:${match.value[2]}cm;height:${match.value[3]}cm"></div>`;
                             this.resource.content = this.resource.content.replace(match.value[0], divElem);
                             skipPosition.push([pos, pos + divElem.length]);
                             a += match.value[0].length;
@@ -75,47 +81,123 @@ new Vue({
 
             return skipPosition;
         },
-        generateRand(){
-            return Math.floor(Math.random()*3+1);
+        generateRand() {
+            return Math.floor(Math.random() * 3 + 1);
         },
-        splitStrings(skipPosition) {
+
+        kermet(useSynonym, skipPosition) {
+            console.log(skipPosition);
+            //first make the strings splittable by omitting renderable content
+            let substringable = []
+
+            let bufferPos = 0;
+            if (skipPosition !== []) {
+                skipPosition.forEach((pos) => {
+                    //console.log(pos);
+                    substringable.push([bufferPos, pos[0]]);
+                    bufferPos = pos[1];
+                });
+                substringable.push([bufferPos, this.resource.content.length]);
+            } else {
+                substringable.push([0, this.resource.content.length]);
+            }
+            console.log(substringable);
+            let DOMOmittedText = "";
+            substringable.forEach((pos) => DOMOmittedText += this.resource.content.substring(pos[0], pos[1]));
+
+            console.log(DOMOmittedText);
+            //randomly select one word
+
+            if (useSynonym === "yes") {
+                //uses words api and and finds synonyms 
+                console.log("You selected yes ");
+            } else {
+
+            }
+
+        },
+        splitStrings(skipPosition, kermetResource) {
             //splits the strings. can't use str.split(" ") function because , there is presence of html elements ready to be rendered.
             //needs a counter to skip certain positions.
 
-            let renderableElements=[]; 
+            let renderableElements = [];
 
             let bufferString = this.resource.content;
             let counter = 0;
-            
-            
-            
-            let enterCounter;
-            let flag = skipPosition == 0 ? true : false;
+
+
+            let flag;
+
+            if (skipPosition.length === 0) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+
             for (let i = 0; i < bufferString.length; i++) {
-                
+
                 if (flag == false) {
                     if (skipPosition[counter][0] == i) {
-                        renderableElements.push(bufferString.substring(skipPosition[counter][0],skipPosition[counter][1]));
+                        renderableElements.push(bufferString.substring(skipPosition[counter][0], skipPosition[counter][1]));
                         i = skipPosition[counter][1];
                         counter++;
                         if (counter == skipPosition.length) {
                             flag = true;
                         }
-                    } 
-                }
-                if(bufferString[i]=='\n'){
-                    enterCounter++;
-                    if(enterCounter >= 2){
-                        renderableElements.push(`<p></p>`);
                     }
-                }else{
-                    enterCounter=0;
                 }
-                renderableElements.push(`<span style="font-family:ash${this.generateRand()};font-size:40px">${bufferString[i]}</span>`);
-                
+
+                //for counting enter and addding a paragraph break if found so.
+                if (bufferString[i] == '\n') {
+                    renderableElements.push(`<p></p>`);
+                   
+                } 
+
+                //kermet controls indentation errors and no of text to cut
+
+                //for bad indentation
+                if (i % (280 - (this.resource.kermetFactor * 2) - 20) == 0) {
+                    renderableElements.push([`<div style="position: relative;float:left;width:20px;height:${this.resource.fontSize}px;background-color:${this.resource.showIndent}"></div>`]);
+                    spanData=`<span style="font-family:ash${this.generateRand()};font-size:${this.resource.fontSize}px;position:relative;'">${bufferString[i]}</span>`;
+                } else {
+                    spanData=`<span style="font-family:ash${this.generateRand()};font-size:${this.resource.fontSize}px;">${bufferString[i]}</span>`;
+                }
+
+                //use different font for punctuation
+                switch(bufferString[i]){
+                    case "{":
+                    case "}":
+                    case "(":
+                    case ")":
+                    case "<":
+                    case ">":
+                    case "#":
+                    case "=":
+                    case "]":
+                    case "[":
+                    case "*":
+                    case ":":
+                    case "\"":
+                    case "'":
+                    case "|":
+                    case "/":
+                    case "&":
+                    case "’":
+                    case "”":
+                    case "“":
+                    case ",":
+                    case ".":
+                    case "-":
+                    case "$":
+                    case "^":
+                    case"\`":
+                    case"~":
+                        spanData = spanData.replace(/ash[0-9]/gm,"punctuationAshish");
+                }
+                renderableElements.push(spanData);
                 //this.resource.content = this.resource.content.replace(bufferString[i],``)
             }
-            
+
             return renderableElements;
         }
 
